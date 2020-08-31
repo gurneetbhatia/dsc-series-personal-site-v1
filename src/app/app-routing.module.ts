2804d5a99c2f91fc4af402c33f895413699ba9d9
Gurneet Bhatia <sbgurneet@gmail.com>
2020-08-31 18:50:17 +0400

    feat: added route for xp page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'experience', component: ExperienceComponent },
   { path: '', component: HomeComponent, pathMatch: 'full' }
 ];
 
