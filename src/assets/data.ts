be499d7716807751ee39787293f6b0b57762dbb3
Gurneet Bhatia <sbgurneet@gmail.com>
2020-09-01 11:04:38 +0400

    feat: defined xp list

---
+++ src/assets/data.ts
@@ -0,0 +1,18 @@
+import { Experience } from './dtos/experience.dto';
+
+export const experiences: Experience[] = [
+    {
+        title: "some experience",
+        location: "some place, narnia",
+        description: "some description",
+        from: "12/07/2017",
+        to: "13/08/2017"
+    },
+    {
+        title: "another experience",
+        location: "the matrix",
+        description: "killing ninjas",
+        from: "15/06/2019",
+        to: "10/09/2019"
+    }
+]
---
+++ src/assets/dtos/experience.dto.ts
@@ -0,0 +1,7 @@
+export interface Experience {
+    title: string;
+    location: string;
+    description: string;
+    from: string;
+    to: string;
+}
