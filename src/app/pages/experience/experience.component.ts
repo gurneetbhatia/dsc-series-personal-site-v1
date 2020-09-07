6a24b0f66a26d545b65ab85965cac7538f6a8d65
Gurneet Bhatia <sbgurneet@gmail.com>
2020-09-07 13:35:44 +0100

    refactor: unsubscribes from the promise

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { DataService } from 'src/app/services/data.service';
 import { Experience } from 'src/assets/dtos/experience.dto';
 import { NotificationService } from 'src/app/services/notification.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
-export class ExperienceComponent implements OnInit {
+export class ExperienceComponent implements OnInit, OnDestroy {
 
   experiences: Experience[];
+
+  private destroy$ = new Subscription();
 
   constructor(private dataService: DataService,
               private notificationService: NotificationService) { }
 
   ngOnInit(): void {
-    this.dataService.getExperiences().subscribe(
+    this.destroy$ = this.dataService.getExperiences().subscribe(
       (experiences) => {
         this.experiences = experiences;
       },
@@ -25,6 +28,10 @@ export class ExperienceComponent implements OnInit {
         this.notificationService.showError("A problem occured fetching the experiences. Please try again later.")
       }
     )
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.unsubscribe();
   }
 
 }
